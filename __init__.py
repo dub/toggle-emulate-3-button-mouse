d74600140df7382ef47afd08e52211e390035345
= <aaron.weyenberg@gmail.com>
2024-09-13 23:57:26 -0400

    Remove __pycache__ from tracking

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import bpy
-from bpy.types import Operator
+import rna_keymap_ui
+from bpy.types import Operator, AddonPreferences
 
 bl_info = {
     "name": "Toggle Emulate 3 Button Mouse",
@@ -10,6 +11,8 @@ bl_info = {
     "description": "Toggles the 'Emulate 3 Button Mouse' preference",
     "category": "User Interface",
 }
+
+addon_keymaps = []
 
 class EMULATE_OT_toggle_three_button_mouse(Operator):
     bl_idname = "emulate.toggle_three_button_mouse"
@@ -29,11 +32,46 @@ class EMULATE_OT_toggle_three_button_mouse(Operator):
         self.report({'INFO'}, f"Emulate 3 Button Mouse: {status}")
 
         return {'FINISHED'}
+    
 
-addon_keymaps = []
+class EmulatePreference(AddonPreferences):
+    bl_idname = __name__
+
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        box = row.box()
+        box.label(text="Keymap")
+        col = box.column()
+
+        wm = bpy.context.window_manager
+        kc = wm.keyconfigs.user
+        old_km_name = ""
+        get_kmi_1 = []
+        for km_add, kmi_add in addon_keymaps:
+            for km_con in kc.keymaps:
+                if km_add.name == km_con.name:
+                    km = km_con
+                    break
+            for kmi_con in km.keymap_items:
+                if kmi_add.idname == kmi_con.idname:
+                    if kmi_add.name == kmi_con.name:
+                        get_kmi_1.append((km,kmi_con))
+
+        get_kmi_1 = sorted(set(get_kmi_1), key=get_kmi_1.index)
+
+        for km, kmi in get_kmi_1:
+            if not km.name == old_km_name:
+                col.label(text=str(km.name), icon="DOT")
+            col.context_pointer_set("keymap", km)
+            rna_keymap_ui.draw_kmi([],kc, km, kmi, col, 0)
+            col.separator()
+            old_km_name = km.name
+            
 
 def register():
     bpy.utils.register_class(EMULATE_OT_toggle_three_button_mouse)
+    bpy.utils.register_class(EmulatePreference)
 
     # Add the keymap
     wm = bpy.context.window_manager
@@ -57,6 +95,7 @@ def unregister():
     addon_keymaps.clear()
 
     bpy.utils.unregister_class(EMULATE_OT_toggle_three_button_mouse)
+    bpy.utils.unregister_class(EmulatePreference)
 
 if __name__ == "__main__":
     register()
